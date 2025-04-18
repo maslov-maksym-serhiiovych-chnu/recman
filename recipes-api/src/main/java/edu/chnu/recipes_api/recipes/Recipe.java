c42849494b03e1ad7ab862aa1d7fdda3133a3947
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2025-04-18 12:24:16 +0300

    create Recipe.java

---
+++ recipes-api/src/main/java/edu/chnu/recipes_api/recipes/Recipe.java
@@ -0,0 +1,16 @@
+package edu.chnu.recipes_api.recipes;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(length = 50, unique = true, nullable = false)
+    private String title;
+    
+    @Column(length = 1000000, unique = true, nullable = false)
+    private String instructions;
+}
