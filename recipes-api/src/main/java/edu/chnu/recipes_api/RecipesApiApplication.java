47857255b03dde5c95277e6d0940bace8255fd2b
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2025-04-18 11:15:19 +0300

    create recipes-api project

---
+++ recipes-api/src/main/java/edu/chnu/recipes_api/RecipesApiApplication.java
@@ -0,0 +1,13 @@
+package edu.chnu.recipes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesApiApplication.class, args);
+	}
+
+}
