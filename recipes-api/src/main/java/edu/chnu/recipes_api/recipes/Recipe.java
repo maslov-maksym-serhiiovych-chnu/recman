afc090f68a4ea49b9a3b4041d29766fc85cd5508
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2025-04-18 13:40:06 +0300

    add constructor and some getters to Recipe.java

--- recipes-api/src/main/java/edu/chnu/recipes_api/recipes/Recipe.java
+++ recipes-api/src/main/java/edu/chnu/recipes_api/recipes/Recipe.java
@@ -7,14 +7,30 @@ public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @Column(length = 50, unique = true, nullable = false)
     private String title;
-    
+
     @Column(length = 1000000, unique = true, nullable = false)
     private String instructions;
 
+    public Recipe() {
+    }
+
+    public Recipe(String title, String instructions) {
+        this.title = title;
+        this.instructions = instructions;
+    }
+
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getInstructions() {
+        return instructions;
     }
 }
