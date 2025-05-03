b6e3e4224c330077fead2acca027cbbea23ba6da
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2025-05-03 21:10:34 +0300

    create UserRepository.java

---
+++ recman-backend/src/main/java/edu/chnu/recman_backend/auth/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package edu.chnu.recman_backend.auth.repositories;
+
+import edu.chnu.recman_backend.auth.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
