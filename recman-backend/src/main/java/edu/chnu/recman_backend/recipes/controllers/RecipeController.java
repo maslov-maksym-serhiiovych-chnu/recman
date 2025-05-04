787f316672dbbd9b74275d2fce54498a8d04ac63
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2025-05-04 15:11:15 +0300

    create RecipeController.java

---
+++ recman-backend/src/main/java/edu/chnu/recman_backend/recipes/controllers/RecipeController.java
@@ -0,0 +1,51 @@
+package edu.chnu.recman_backend.recipes.controllers;
+
+import edu.chnu.recman_backend.recipes.dtos.RecipeCreateRequest;
+import edu.chnu.recman_backend.recipes.dtos.RecipeDetails;
+import edu.chnu.recman_backend.recipes.dtos.RecipeListItem;
+import edu.chnu.recman_backend.recipes.dtos.RecipeUpdateRequest;
+import edu.chnu.recman_backend.recipes.services.RecipeService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/recman/recipes")
+public class RecipeController {
+    private final RecipeService service;
+
+    public RecipeController(RecipeService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<RecipeDetails> create(@Valid @RequestBody RecipeCreateRequest request) {
+        RecipeDetails created = service.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RecipeListItem>> readAll() {
+        return ResponseEntity.ok(service.readAll());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<RecipeDetails> read(@PathVariable Long id) {
+        return ResponseEntity.ok(service.read(id));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> update(@PathVariable Long id, @Valid @RequestBody RecipeUpdateRequest request) {
+        service.update(id, request);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
