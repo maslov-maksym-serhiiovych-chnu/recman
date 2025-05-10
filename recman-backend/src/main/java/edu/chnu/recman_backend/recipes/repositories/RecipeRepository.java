2688837627777ce0e61cdc325dfa6e1263e29574
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2025-05-10 14:50:08 +0300

    update RecipeRepository.java to include user-specific query

--- recman-backend/src/main/java/edu/chnu/recman_backend/recipes/repositories/RecipeRepository.java
+++ recman-backend/src/main/java/edu/chnu/recman_backend/recipes/repositories/RecipeRepository.java
@@ -1,10 +1,18 @@
 package edu.chnu.recman_backend.recipes.repositories;
 
+import edu.chnu.recman_backend.auth.models.User;
 import edu.chnu.recman_backend.recipes.models.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-    boolean existsByName(String name);
+    boolean existsByNameAndUser(String name, User user);
+    
+    List<Recipe> findAllByUser(User user);
+    
+    Optional<Recipe> findByIdAndUser(Long id, User user);
 }
