775ef01c95405f4c7a01bebb13be965503686a12
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2025-05-10 10:53:01 +0300

    refactor RecmanExceptionHandler.java

--- recman-backend/src/main/java/edu/chnu/recman_backend/handlers/RecmanExceptionHandler.java
+++ recman-backend/src/main/java/edu/chnu/recman_backend/handlers/RecmanExceptionHandler.java
@@ -5,6 +5,7 @@ import edu.chnu.recman_backend.recipes.exceptions.RecipeAlreadyExistsException;
 import edu.chnu.recman_backend.recipes.exceptions.RecipeNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,6 +16,11 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class RecmanExceptionHandler {
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String, String>> handleBadCredentials(BadCredentialsException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", ex.getMessage()));
+    }
+
     @ExceptionHandler(UsernameAlreadyExistsException.class)
     public ResponseEntity<Map<String, String>> handleUsernameExists(UsernameAlreadyExistsException ex) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("error", ex.getMessage()));
