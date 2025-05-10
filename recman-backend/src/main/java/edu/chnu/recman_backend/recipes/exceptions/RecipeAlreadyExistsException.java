058a76f41d44b2010d712463389f6ff403a84483
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2025-05-10 12:55:17 +0300

    rename RecipeAlreadyExistsException.java

--- recman-backend/src/main/java/edu/chnu/recman_backend/recipes/exceptions/RecipeAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package edu.chnu.recman_backend.recipes.exceptions;
-
-public class RecipeAlreadyExistsException extends RuntimeException {
-    public RecipeAlreadyExistsException() {
-        super("Recipe already exists");
-    }
-}
