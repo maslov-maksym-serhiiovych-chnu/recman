8854344d79943e69051237f0b5e832584776eddd
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2025-05-17 19:37:52 +0300

    refactor(auth-responses): remove LoginResponse.java and RegisterResponse.java

--- recman-backend/src/main/java/edu/chnu/recman_backend/auth/dtos/LoginResponse.java
+++
@@ -1,4 +0,0 @@
-package edu.chnu.recman_backend.auth.dtos;
-
-public record LoginResponse(String token) {
-}
--- recman-backend/src/main/java/edu/chnu/recman_backend/auth/dtos/RegisterResponse.java
+++
@@ -1,4 +0,0 @@
-package edu.chnu.recman_backend.auth.dtos;
-
-public record RegisterResponse(String username) {
-}
