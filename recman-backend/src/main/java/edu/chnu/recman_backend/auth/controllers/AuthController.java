51025ea3562c3844ab6202f89235a55f3f53e36a
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2025-05-18 12:23:35 +0300

    feat(validate-token): add token validation endpoint to AuthController.java

--- recman-backend/src/main/java/edu/chnu/recman_backend/auth/controllers/AuthController.java
+++ recman-backend/src/main/java/edu/chnu/recman_backend/auth/controllers/AuthController.java
@@ -6,10 +6,7 @@ import edu.chnu.recman_backend.auth.services.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/recman/auth")
@@ -26,5 +23,10 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<String> login(@Valid @RequestBody LoginRequest request) {
         return ResponseEntity.ok(service.login(request));
+    }
+
+    @GetMapping("/validate")
+    public ResponseEntity<Void> validate() {
+        return ResponseEntity.ok().build();
     }
 }
